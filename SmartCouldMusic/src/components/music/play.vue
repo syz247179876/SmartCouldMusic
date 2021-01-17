9cac1efb618979bc23b6392f096aa85a8134cc94
syz <syz247179876@126.com>
2021-01-17 12:38:59 +0800

    1.修复歌曲播放界面歌词和音乐不匹配的bug.2.修复播放结束停止动画

--- SmartCouldMusic/src/components/music/play.vue
+++ SmartCouldMusic/src/components/music/play.vue
@@ -9,30 +9,34 @@
       ref="audio"
     ></audio>
     <div class="img-div">
-      <img :class="{'play-img': isPlaying}" src="../../../static/img/needle-ip6.png" width="100px">
+      <img
+        :class="{ 'play-img': status }"
+        src="../../../static/img/needle-ip6.png"
+        width="100px"
+      />
     </div>
     <div class="cd">
       <img
         v-if="detail.al"
-        :class="['midpic', isPlaying ? 'cd-animation' : 'cd-stop']"
+        :class="['midpic', status ? 'cd-animation' : 'cd-stop']"
         :src="detail.al.picUrl"
         width="188px"
         @click="play"
+        id="img_cd"
       />
     </div>
     <div class="lrc-div">
       <!-- offsetH滚出页面的高度 -->
       <div
         class="lrc-content"
-        :style="{ transform: 'translateY(' + offsetH + 'px)' }"
+        :style="{ transform: 'translateY(' + offset + 'px)' }"
       >
         <p v-if="detail.ar">{{ detail.name }}-{{ detail.ar[0].name }}</p>
-        <p
-          v-for="(item, key, index) in finalLrc"
-          :key="index"
-          :class="['lrc', { current: key == nowTime }]"
-        >
-          {{ item }}
+
+        <p v-for="(item, index) in lyricDict" :key="index">
+          <span :class="[{ helight: index == lyricList[currentLyc] }]">
+            {{ item }}
+          </span>
         </p>
       </div>
     </div>
@@ -44,10 +48,14 @@ export default {
   data() {
     return {
       finalLrc: {},
-      offsetH: 20,
+      offset: -1,
       nowTime: "00:00",
+      lyricDict: {}, // 时间歌词键值对
+      lyricList: [], // 存储时间列表
       detail: {},
-      isPlaying: false,
+      currentLyc: 0, // 对应第几句歌词
+      status: false,
+      lrcHegiht: 11,
     };
   },
   created() {
@@ -60,7 +68,7 @@ export default {
       this.$http("api/lyric", { params: { id } })
         .then((res) => {
           let lyric = res.data.lrc.lyric;
-          this.finalLrc = this.transLrc(lyric);
+          this.transLrc(lyric); // 歌词
         })
         .catch((err) => {
           console.log(err);
@@ -75,45 +83,90 @@ export default {
           console.log(err);
         });
     },
-    // 格式化歌词
-    transLrc(lrc) {
-      let obj = {};
-      let reg = /\[(.*)](.*)/g;
-      lrc.replace(reg, (a, b, c) => {
-        // a是匹配到的内容
-        // b是时间，精确到秒；是第一个子表达式的内容
-        b = b.slice(0, 5);
-        // c是歌词的内容；是第二个子表达式的内容
-        obj[b] = c;
+    // 将字符串转为字典
+    transLrc(lyric) {
+      let regex = /\[(.*)](.*)/g;
+      lyric.replace(regex, (whole, first, second) => {
+        // {time: lrc}  时间:歌词键值对
+        first = this.computeTime(first.slice(0, 5));
+        this.lyricDict[first] = second;
+        this.lyricList.push(first);
       });
-      return obj;
     },
+    // 计算时间 --> 秒
+    computeTime(timeString) {
+      let min = timeString.slice(0, 2); // 切出分钟
+      let sec = timeString.slice(3, 5); // 切出秒钟
+      return parseInt(min) * 60 + parseInt(sec);
+    },
+    // // 格式化歌词
+    // transLrc(lrc) {
+    //   let obj = {};
+    //   let reg = /\[(.*)](.*)/g;
+    //   lrc.replace(reg, (a, b, c) => {
+    //     // a是匹配到的内容
+    //     // b是时间，精确到秒；是第一个子表达式的内容
+    //     b = b.slice(0, 5);
+    //     // c是歌词的内容；是第二个子表达式的内容
+    //     obj[b] = c;
+    //   });
+    //   return obj;
+    // },
+
     // 播放/暂停音乐
     play() {
       let oAudio = this.$refs.audio;
-      if (oAudio.paused) {
+      if (!this.status) {
+        this.status = true;
         oAudio.play();
-        this.isPlaying = true;
         oAudio.ontimeupdate = () => {
-          // 转换时间格式  100s => 1min40s
-          this.nowTime = this.tranTime(oAudio.currentTime);
-          if (this.nowTime in this.finalLrc) {
-            this.offsetH -= 10;
+          if (parseInt(oAudio.currentTime) in this.lyricDict) {
+            this.currentLyc = this.lyricList.indexOf(
+              parseInt(oAudio.currentTime)
+            );
+            this.offset -= this.lrcHegiht;
           }
         };
-      }else {
+      } else {
+        this.status = false;
         oAudio.pause();
-        this.isPlaying = false;
       }
+      // if (oAudio.paused) {
+      //   oAudio.play();
+      //   this.isPlaying = true;
+      //   oAudio.ontimeupdate = () => {
+      //     // 转换时间格式  100s => 1min40s
+      //     this.nowTime = this.tranTime(oAudio.currentTime);
+      //     if (this.nowTime in this.finalLrc) {
+      //       this.offsetH -= 10;
+      //     }
+      //   };
+      // } else {
+      //   oAudio.pause();
+      //   this.isPlaying = false;
+      // }
     },
-    // 转换时间格式  100s => 1min40s
-    tranTime(time) {
-      // 分钟
-      // padStart字符串的方法，补位。第一个参数是总共几位，第二个参数是用什么内容来补
-      let min = (Math.floor(time / 60) + "").padStart(2, "0");
-      // 秒数
-      let sec = (Math.floor(time % 60) + "").padStart(2, "0");
-      return `${min}:${sec}`;
+    // // 转换时间格式  100s => 1min40s
+    // tranTime(time) {
+    //   // 分钟
+    //   // padStart字符串的方法，补位。第一个参数是总共几位，第二个参数是用什么内容来补
+    //   let min = (Math.floor(time / 60) + "").padStart(2, "0");
+    //   // 秒数
+    //   let sec = (Math.floor(time % 60) + "").padStart(2, "0");
+    //   return `${min}:${sec}`;
+    // },
+    // 播放结束
+  },
+  watch: {
+    currentLyc() {
+      console.log(this.currentLyc >= this.lyricList.length - 1);
+      if (this.currentLyc >= this.lyricList.length - 1) {
+        let img_cd = document.getElementById("img_cd");
+        img_cd.classList.remove("cd-animation");
+        setInterval(function () {
+          this.status = false;
+        }, 6000);
+      }
     },
   },
 };
@@ -177,16 +230,20 @@ export default {
   position: relative;
 }
 .img-div img {
-    position: fixed;
-    left: 48%;
-    top: 0;
-    transform: rotate(-15deg);
-    transform-origin: 0 0;
-    transition: transform 1s linear;
-    z-index: 999;
+  position: fixed;
+  left: 48%;
+  top: 0;
+  transform: rotate(-15deg);
+  transform-origin: 0 0;
+  transition: transform 1s linear;
+  z-index: 999;
 }
 /* 播放音乐时触针的动画 */
 .img-div img.play-img {
   transform: rotate(0deg);
+}
+
+.helight {
+  color: aqua;
 }
 </style>
